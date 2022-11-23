5f0b29e7b1792f61abfcd3f65ac9d2a3760c2601
Jaya Pradhisha <99787416+Pradhis@users.noreply.github.com>
2022-11-23 21:47:17 +0530

    Add files via upload

---
+++ recursive fact.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int fact(int n)
+{
+    if
+        (n==1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n*fact(n-1);
+    }
+}int main()
+{
+    int n;
+    printf("Entee num:\n");
+    scanf("%d",&n);
+    printf("fact is:%d",fact(n));
+    return 0;
+}
